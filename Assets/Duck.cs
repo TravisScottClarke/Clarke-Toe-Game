88abcd518b605a6a5e6cd892a191fb631eae1efc
ClarkeToes <tdwes046@learner.dvusd.org>
2021-09-23 09:09:07 -0700

    wqerweqweq

---
+++ Assets/Duck.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Duck : MonoBehaviour
+{
+    public bool activate;
+    public float speed;
+    public GameObject plr;
+    public int duk;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(activate == true)
+        {
+            if (duk == 1)
+            {
+
+            }
+            if (duk == 2)
+            {
+
+            }
+            if (duk == 3)
+            {
+
+            }
+            if (duk == 4)
+            {
+
+            }
+            if (duk == 5)
+            {
+
+            }
+            if (duk == 6)
+            {
+
+            }
+
+        }
+    }
+}
